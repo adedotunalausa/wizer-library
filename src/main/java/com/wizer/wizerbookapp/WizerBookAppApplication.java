65213818b00f3576bf0f86f8d2fe2b5ab56d4091
Adedotun Alausa <adedotunalausa@gmail.com>
2021-11-03 03:25:37 +0100

    Initialize project

---
+++ src/main/java/com/wizer/wizerbookapp/WizerBookAppApplication.java
@@ -0,0 +1,13 @@
+package com.wizer.wizerbookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WizerBookAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WizerBookAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wizer/wizerbookapp/WizerBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wizer.wizerbookapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WizerBookAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
